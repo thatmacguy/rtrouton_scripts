3632df1e2fa26a3629b159e2dde407361af13d53
thatmacguy <thatmacguy@users.noreply.github.com>
2021-02-09 13:38:28 -0800

    Update detect_missing_secure_token.sh
    Added validation and support of OS Major versions greater than 10.

--- rtrouton_scripts/Casper_Extension_Attributes/detect_missing_secure_token/detect_missing_secure_token.sh
+++ rtrouton_scripts/Casper_Extension_Attributes/detect_missing_secure_token/detect_missing_secure_token.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-# Jamf Pro Extension Attribute which reports if the
-# current logged-in user has a Secure Token attribute
-# associated with their account.
+# Script which reports if the current logged-in user has a
+# Secure Token attribute associated with their account.
 
 # Determine OS version
 # Save current IFS state
@@ -15,7 +14,7 @@ IFS='.' read osvers_major osvers_minor osvers_dot_version <<< "$(/usr/bin/sw_ver
 
 IFS=$OLDIFS
 
-result=0
+result=5
 
 MissingSecureTokenCheck() {
 
@@ -25,12 +24,12 @@ MissingSecureTokenCheck() {
 
 	# This function checks if the logged-in user has Secure Token attribute associated
 	# with their account. If the token_status variable returns "0", then the following
-	# status is returned from the Extension Attribute:
+	# status is returned from the script:
 	#
 	# 1
 	#
 	# If anything else is returned, the following status is
-	# returned from the Extension Attribute:
+	# returned from the script:
 	#
 	# 0
 
@@ -43,21 +42,22 @@ MissingSecureTokenCheck() {
 		# If there is no secure token associated with the logged-in account,
 		# the token_status variable should return "0".
 
-		if [[ "$token_status" -eq 0 ]]; then
+		if [[ "$token_status" -eq 1 ]]; then
 			result=1
 		fi
+		else result=4
 	fi
 
 	# If unable to determine the logged-in user
 	# or if the logged-in user is root, then the following
-	# status is returned from the Extension Attribute:
+	# status is returned from the script:
 	#
-	# 0
+	# 2, 3 or 4
 }
 
 # Check to see if the OS version of the Mac supports running APFS boot volumes.
 
-if [[ ( ${osvers_major} -eq 10 && ${osvers_minor} -ge 13 ) ]]; then
+if [[ ( ${osvers_major} -eq 10 && ${osvers_minor} -ge 13 ) || ${osvers_major} -ge 11 ]]; then
 
 	# If the OS check passes, check to see if the boot volume has an APFS filesystem
 	# with FileVault turned on.
@@ -67,11 +67,15 @@ if [[ ( ${osvers_major} -eq 10 && ${osvers_minor} -ge 13 ) ]]; then
 		# If the boot volume is using APFS for its filesystem and FileVault is on,
 		# run the MissingSecureTokenCheck function.
 		MissingSecureTokenCheck
+        else
+        result=3
 	fi
 
 	# If the OS, filesystem or encryption check did not pass, the script sets the following string for the "result" value:
 	#
-	# 0
+	# 2
+    else 
+    result=2
 fi
 
 echo "<result>$result</result>"
